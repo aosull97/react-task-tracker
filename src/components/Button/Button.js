2bd3a98ce5de40e627b3a7de482177ef74ad2a6a
aosull97 <149597356+aosull97@users.noreply.github.com>
2024-01-29 12:51:45 +0000

    Add files via upload

---
+++ src/components/Button/Button.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = (props) => {
+  return (
+    <div>
+        <button className='btn' style={{backgroundColor: props.color}} onClick={props.onClick}>{props.text}</button>
+    </div> 
+  )
+}
+
+export default Button
